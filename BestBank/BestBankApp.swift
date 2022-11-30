f7ea7fb1e76bbdc49981c036814ef2772e85b553
CJ <kubachod@gmail.com>
2022-11-30 11:43:59 +0100

    Initial Commit

---
+++ BestBank/BestBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestBankApp.swift
+//  BestBank
+//
+//  Created by Jakub Chodara on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BestBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
